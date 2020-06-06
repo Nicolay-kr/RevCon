078f134ce945a4463f9a2adf1ba8e09b7d466524
Nicolay <nicolay.krischenovich@gmail.com>
2020-06-06 22:51:11 +0300

    add Team block,correct service card,

--- myscript.js
+++ myscript.js
@@ -6,6 +6,7 @@ let toggle = document.querySelector('.toggle');
 let nav_toggle_off = document.querySelector('.nav_toggle_off');
 // let nav = document.querySelector('.nav');
 let services = document.querySelector('.services')
+let brand = document.querySelector('.brand')
 
 window.onscroll = function () {
   if (window.pageYOffset > 1000 && window.pageYOffset<2500) {
@@ -15,11 +16,22 @@ window.onscroll = function () {
     a.classList.remove('contentBxScrol');
     b.classList.remove('imgBxScrol');
   }
-  if (window.pageYOffset >200 && window.pageYOffset<1500) {
+  if (window.pageYOffset >200 && window.pageYOffset<1700) {
 
     services.classList.add('servicesScrol');
   } else {
     services.classList.remove('servicesScrol')
+  }
+  if (window.pageYOffset >3300 && window.pageYOffset<3800) {
+    brand.classList.add('brandScrol');
+    // brand:nth-child(2).classList.add('brandScrol');
+    // brand:nth-child(3).classList.add('brandScrol');
+    // brand:nth-child(4).classList.add('brandScrol');
+  } else {
+    brand.classList.remove('brandScrol');
+    // brand:nth-child(2).classList.remove('brandScrol');
+    // brand:nth-child(3).classList.remove('brandScrol');
+    // brand:nth-child(4).classList.remove('brandScrol');
   }
 
 };
